79fc778dca1e50f86cb910b26be10714cee47e34
barinov274 <barinov274@gmail.com>
2022-07-18 16:28:28 +1000

    change application title

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,41 +1,65 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QFileDialog>
-#include <QProcess>
-#include <QThread>
+#include <QScrollBar>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    settings.beginGroup("pathes");
+    ui->nllbInferencePath->setText(settings.value("nllbInferencePath", "").toString());
+    ui->modelPath->setText(settings.value("modelPath", "").toString());
+    settings.endGroup();
+    settings.beginGroup("languages");
+    ui->sourceLanguage->setCurrentIndex(settings.value("sourceLanguage", 0).toInt());
+    ui->targetLanguage->setCurrentIndex(settings.value("targetLanguage", 0).toInt());
+    settings.endGroup();
+    settings.beginGroup("lastRun");
+    ui->source->setPlainText(settings.value("sourceText", "").toString());
+    ui->target->setPlainText(settings.value("targetText", "").toString());
+    settings.endGroup();
 }
 
 MainWindow::~MainWindow()
 {
+    settings.beginGroup("pathes");
+    settings.setValue("nllbInferencePath", ui->nllbInferencePath->text());
+    settings.setValue("modelPath", ui->modelPath->text());
+    settings.endGroup();
+    settings.beginGroup("languages");
+    settings.setValue("targetLanguage", ui->targetLanguage->currentIndex());
+    settings.setValue("sourceLanguage", ui->sourceLanguage->currentIndex());
+    settings.endGroup();
+    settings.beginGroup("lastRun");
+    settings.setValue("sourceText", ui->source->toPlainText());
+    settings.setValue("targetText", ui->target->toPlainText());
+    settings.endGroup();
+
     delete ui;
 }
 
 
 void MainWindow::on_pushButton_clicked()
 {
-    QProcess c_output;
-    c_output.setProcessChannelMode(QProcess::MergedChannels);
-    c_output.setWorkingDirectory("/run/media/max/02c10692-598c-4d07-a7fa-44a11c161337/home/max/content//sentencepiece/build/src/");
-    c_output.start("bash", QStringList() << "/run/media/max/02c10692-598c-4d07-a7fa-44a11c161337/home/max/content/NLLB-inference/translate.sh" << "/run/media/max/02c10692-598c-4d07-a7fa-44a11c161337/home/max/content/checkpoint.pt" << "jpn_Jpan rus_Cyrl" << "<<< '今日もキメてくネガティ'");
-    if(!c_output.waitForStarted()){
-        ui->target->setPlainText("Failed to tarnslate");
-    }
-    c_output.waitForFinished();
+    QString formatedSource = ui->source->toPlainText().toUtf8().toBase64(); // because of quotes and other symbols
 
-    c_output.write("...");
-    c_output.closeWriteChannel();
+    //QString comand = "bash "+ui->nllbInferencePath->text()+" "+ui->modelPath->text()+" "+languages[ui->sourceLanguage->currentText()]+" "+languages[ui->targetLanguage->currentText()]+" <<< '"+formatedSource+"'";
+    QString comand = "bash "+ui->nllbInferencePath->text()+" "+ui->modelPath->text()+" "+languages[ui->sourceLanguage->currentText()]+" "+languages[ui->targetLanguage->currentText()]+" <<< `echo '"+formatedSource+"' | base64 --decode`";
+    ui->target->setPlainText("sh -c "+comand+"\n");
+    ui->target->verticalScrollBar()->setValue(ui->target->verticalScrollBar()->maximum());
 
-    while(c_output.state() != QProcess::NotRunning){
-        //ui->target->setPlainText(".");
-        if(c_output.waitForReadyRead())
-            ui->target->setPlainText(ui->target->toPlainText()+c_output.readAllStandardOutput());
-    }
+    c_output =  new QProcess(this);
+    connect(c_output, SIGNAL(readyReadStandardOutput()), this, SLOT(processOutput()));
+    connect(c_output, SIGNAL(readyReadStandardError()), this, SLOT(processOutput()));
+    c_output->start("sh", QStringList() << "-c" << comand);
+}
+
+void MainWindow::processOutput(){
+    ui->target->setPlainText(ui->target->toPlainText()+c_output->readAllStandardOutput());
+    ui->target->setPlainText(ui->target->toPlainText()+c_output->readAllStandardError());
+    ui->target->verticalScrollBar()->setValue(ui->target->verticalScrollBar()->maximum());
 }
 
 
